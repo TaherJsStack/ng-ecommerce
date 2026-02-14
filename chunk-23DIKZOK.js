3743cdf77b994a724f4ea1894694ac66c67474b0
Taher Ahmed <Taher.Ahmed@softec.ai>
2026-02-15 00:51:39 +0200

    Auto-generated commit

--- chunk-23DIKZOK.js
+++
@@ -1,1 +0,0 @@
-import{o as t,p as s}from"./chunk-TSZB7BAR.js";var r=s("auth"),u=t(r,e=>e.user?e.user:null),n=t(r,e=>e.isAuthenticated),l=t(r,e=>e.isAuthenticated);export{u as a,n as b};
---
+++ chunk-CU6LLK34.js
@@ -0,0 +1,1 @@
+import{a as t}from"./chunk-BKMCY3JI.js";var d=t({selectId:r=>r.id?.toString()??""});export{d as a};
